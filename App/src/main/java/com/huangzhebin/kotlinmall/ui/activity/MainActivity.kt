e386a9fbade32fb87caf8dc41774f00502ca6f4d
黄哲彬 <huangzb@fastbank.net>
2018-12-11 15:01:59 +0800

    Initial commit

---
+++ App/src/main/java/com/huangzhebin/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.huangzhebin.kotlinmall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.huangzhebin.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ UserCenter/src/main/java/com/huangzhebin/usercenter/ui/activity/RegisteActivity.kt
@@ -0,0 +1,13 @@
+package com.huangzhebin.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.huangzhebin.usercenter.R
+
+class RegisteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registe)
+    }
+}
